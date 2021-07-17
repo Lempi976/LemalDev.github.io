a0056854348e432c4080473741525545d1d4465a
Lempi976 <58383746+Lempi976@users.noreply.github.com>
2021-07-17 12:58:56 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,17 @@
+
+     	function openPage() {
+            var x = document.getElementById("search").value;
+
+            if (x === "dog") {
+                window.open("/index3.html");
+            }
+
+            if (x === "cat") {
+                window.open("/index4.html");
+            }
+	    if (x === "cat") {
+                window.open("/index5.html");
+            }
+
+        }
+
