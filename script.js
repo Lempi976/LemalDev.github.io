273fb28b3192a0ffcc494e3e48fd6ab2c58dee5a
Lempi976 <58383746+Lempi976@users.noreply.github.com>
2023-09-02 21:06:32 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.querySelectorAll('.buy-button').forEach(function(button) {
+    button.addEventListener('click', function() {
+        alert('Thank you for your purchase!');
+    });
+});
